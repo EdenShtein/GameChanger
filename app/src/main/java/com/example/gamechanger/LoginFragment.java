42ecf6fda40e57a5b964fbc1c94587a23c096e63
אור לוי <levior9@gmail.com>
2021-01-07 13:33:54 +0200

    sign in to sign up action

---
+++ app/src/main/java/com/example/gamechanger/LoginFragment.java
@@ -0,0 +1,29 @@
+package com.example.gamechanger;
+
+import android.os.Bundle;
+
+import androidx.fragment.app.Fragment;
+import androidx.navigation.Navigation;
+
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.Button;
+
+public class LoginFragment extends Fragment {
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        // Inflate the layout for this fragment
+        View view = inflater.inflate(R.layout.fragment_login, container, false);
+        Button SignUpBtn = view.findViewById(R.id.signin_signup_btn);
+        SignUpBtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Navigation.findNavController(view).navigate(R.id.action_login_to_signUp);
+            }
+        });
+        return view;
+    }
+}
