74dfc62b27a58964c7611904b783645c0c3a4bb9
Eden Shtein <edenshtein30@cs.colman.ac.il>
2021-01-07 17:11:01 +0200

    minor changes

--- app/src/main/java/com/example/gamechanger/SignUpFragment.java
+++ app/src/main/java/com/example/gamechanger/SignUpFragment.java
@@ -3,20 +3,28 @@ package com.example.gamechanger;
 import android.os.Bundle;
 
 import androidx.fragment.app.Fragment;
+import androidx.navigation.Navigation;
 
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.TextView;
 
 
 public class SignUpFragment extends Fragment {
-
+    TextView signinLink;
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         // Inflate the layout for this fragment
         View view = inflater.inflate(R.layout.fragment_sign_up, container, false);
-
+        signinLink= view.findViewById(R.id.signin_link);
+        signinLink.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Navigation.findNavController(view).navigate(R.id.action_signup_to_signin);
+            }
+        });
         return view;
     }
 }
