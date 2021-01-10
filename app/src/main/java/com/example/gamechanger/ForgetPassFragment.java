e9d9c920b3e1ad188c9af011d999f9f0c46332af
אור לוי <levior9@gmail.com>
2021-01-10 11:05:42 +0200

    Forget password page

--- app/src/main/java/com/example/gamechanger/ForgetPassFragment.java
+++ app/src/main/java/com/example/gamechanger/ForgetPassFragment.java
@@ -7,15 +7,26 @@ import androidx.fragment.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
 
 
 public class ForgetPassFragment extends Fragment {
 
+    Button resetBtn;
+    EditText email;
 
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_forget_pass, container, false);
+        View view = inflater.inflate(R.layout.fragment_forget_pass, container, false);
+
+        resetBtn = view.findViewById(R.id.forgetpass_reset_btn);
+        email = view.findViewById(R.id.forgetpass_email_input);
+
+
+        return view;
     }
 }
