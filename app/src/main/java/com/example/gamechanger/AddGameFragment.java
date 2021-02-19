31f4973035236a7d367efada6b4f5b7b5d690faf
Omer Berger <oberger96@cs.colman.ac.il>
2021-02-19 17:43:09 +0200

    remove blank spaces

--- app/src/main/java/com/example/gamechanger/AddGameFragment.java
+++ app/src/main/java/com/example/gamechanger/AddGameFragment.java
@@ -78,12 +78,10 @@ public class AddGameFragment extends Fragment {
         gamePrice.addTextChangedListener(new TextWatcher() {
             @Override
             public void beforeTextChanged(CharSequence s, int start, int count, int after) {
-
             }
 
             @Override
             public void onTextChanged(CharSequence s, int start, int before, int count) {
-
             }
 
             @Override
